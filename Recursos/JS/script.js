6ed2ad8e90a733fe621dcdd60502dc92fa846229
Hans Stöhr <41397975+Hansstohr@users.noreply.github.com>
2023-03-08 17:10:31 -0300

    Initial Commit

---
+++ Recursos/JS/script.js
@@ -0,0 +1,7 @@
+document.getElementById("mostrar").addEventListener("click", function() {
+    var div = document.getElementById("mas-texto");
+    div.style.display = "block"; // o "inline"
+
+    var div2 = document.getElementById("mostrar");
+    div2.style.display = "none";
+});
